3982b1eb85a1bbb9c0fe6129419e7bdfb17d3f24
Murat Dinc <muratdinc.md@gmail.com>
2023-10-08 20:05:06 +0300

    Changed the structure of the app to fix some bugs. Application icon has been added.

---
+++ Build SS/README.md
@@ -0,0 +1,1 @@
+If no visual changes are made in the new version, no new ekrabn image is added. The most recent version available here looks the same as the current version.
---
+++ my-text-reader/my_text_readerApp.swift
@@ -0,0 +1,18 @@
+//
+//  my_text_readerApp.swift
+//  my-text-reader
+//
+//  Created by Murat Dinç on 26.09.2023.
+//  DivWizard - divwizard.com
+//
+
+import SwiftUI
+
+@main
+struct my_text_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
